0530c9dd6df7870a13994c5053d60c63910e3aff
Gamb25 <143144941+Gamb25@users.noreply.github.com>
2023-10-29 22:37:01 -0500

    Parcial2.pro
    Se creó le nuevo proyecto en la carpeta del parcial, ya se puede verificar la matriz, mañana se deben empezar a implementar las reglas de de movimientos validos para el juego

---
+++ Parcial2/ficha.h
@@ -0,0 +1,19 @@
+#ifndef FICHA_H
+#define FICHA_H
+
+#include <iostream>
+using namespace std;
+
+class ficha
+{
+public:
+    ficha();
+    void set_color(char _color);
+    char get_color();
+
+private:
+    char color;
+
+};
+
+#endif // FICHA_H
---
+++ Parcial2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <tablero.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    tablero juego1;
+    juego1.mostrar_matriz_tablero();
+
+
+    return 0;
+}
