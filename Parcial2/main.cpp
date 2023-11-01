5a38e0d95df17e4adb366654977257b65ebd2452
Gamb25 <143144941+Gamb25@users.noreply.github.com>
2023-10-31 21:54:30 -0500

    Revert "tablero.h tablero.cpp Parcial2\tablero.h Parcial2\tablero.cpp Parcial2\Parcial2.pro Parcial2\main.cpp Parcial2\ficha.h Parcial2\ficha.cpp Parcial2\.gitignore Parcial2.pro.user Parcial2.pro main.cpp ficha.h ficha.cpp build-Parcial2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\Makefile.Release build-Parcial2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\Makefile.Debug build-Parcial2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\Makefile build-Parcial2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\tablero.o build-Parcial2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\Parcial2.exe build-Parcial2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\main.o build-Parcial2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\ficha.o build-Parcial2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\.qmake.stash"
    This reverts commit 19c032edff7a1d77c6f1426220eccb5350e44472.

--- Parcial2/main.cpp
+++ Parcial2/main.cpp
@@ -8,9 +8,7 @@ int main()
 {
     tablero juego1;
     juego1.mostrar_matriz_tablero();
-    juego1.mover_ficha(3,3,3,5,'-');
 
-    juego1.mostrar_matriz_tablero();
 
     return 0;
 }
--- Parcial2/tablero.h
+++ Parcial2/tablero.h
@@ -8,8 +8,6 @@ class tablero : public ficha
 public:
     tablero();
     ~tablero();
-    void agregar_ficha(int _fila,int _columna,char _color);
-    void mover_ficha(int _fila_actual,int _columna_actual,int _fila_destino,int _columna_destino,char _color);
     int get_dimension_tablero();
     void mostrar_matriz_tablero();
 
